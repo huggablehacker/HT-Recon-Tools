19c044d4ba5b17f68ad9e23df0f4c60746893e93
Huggable Hacker <huggablehacker@users.noreply.github.com>
2018-05-08 16:11:54 -0400

    Create PSRT.sh

---
+++ PSRT.sh
@@ -0,0 +1,95 @@
+#!/bin/bash
+
+clear
+echo HHs Awsome PS Script to do cool recon on websites. Using the HackerTarget.com API.
+
+if [ -z "$1" ]; then
+
+                echo "[*] Usage : $0 <Enter your target domain, ie google.com> "
+
+                exit 0
+
+fi
+
+PS3='Please enter your choice: '
+
+options=("TraceRt" "Test Ping" "DNS Lookup" "DNS Host Records" "Reverse DNS" "Whois Lookup" "GEO IP" "Reverse IP" "HTTP Header Check" "Extract Links from Page" "Quit")
+
+select opt in "${options[@]}"
+
+do
+
+    case $opt in
+
+                                                "TraceRt")
+
+            curl "https://api.hackertarget.com/mrt/?q=$1" > tracert-$1.txt
+
+            ;;
+
+                                                "Test Ping")
+
+            curl "https://api.hackertarget.com/nping/?q=$1" > ping-$1.txt
+
+            ;;
+
+                                                "DNS Lookup")
+
+            curl "https://api.hackertarget.com/dnslookup/?q=$1" > dns-lookup-$1.txt
+
+            ;;
+
+                                                "DNS Host Records")
+
+            curl "https://api.hackertarget.com/hostsearch/?q=$1" > $1.txt
+
+            ;;
+
+                                                "Reverse DNS")
+
+            curl "https://api.hackertarget.com/reversedns/?q=$1" > reverse-dns-$1.txt
+
+            ;;
+
+                                                "Whois Lookup")
+
+            curl "https://api.hackertarget.com/whois/?q=$1" > whois-$1.txt
+
+            ;;
+
+                                                "GEO IP")
+
+            curl "https://api.hackertarget.com/geoip/?q=$1" > geo-ip-$1.txt
+
+            ;;
+
+                                                "Reverse IP")
+
+            curl "https://api.hackertarget.com/reverseiplookup/?q=$1" > reverse-ip-$1.txt
+
+            ;;
+
+                                                "HTTP Header Check")
+
+            curl "https://api.hackertarget.com/httpheaders/?q=$1" > http-header-$1.txt
+
+            ;;
+
+                                                "Extract Links from Page")
+
+            curl "https://api.hackertarget.com/pagelinks/?q=$1" > page-link-$1.txt
+
+            ;;
+						"Quit")
+
+
+            break
+
+            ;;
+
+        *) echo invalid option;;
+
+    esac
+
+done
+clear
